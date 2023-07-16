0452215ad600a28c813936884222b2dc875490b3
gandhyman <gandhycoder@gmail.com>
2023-07-16 00:28:43 -0600

    front

---
+++ components/CartContext.js
@@ -0,0 +1,48 @@
+import { set } from 'mongoose';
+
+const { createContext, useState, useEffect } = require('react');
+
+export const CartContext = createContext({});
+
+export function CartContextProvider({ children }) {
+  // We check if exist local storage data
+  const ls = typeof window !== 'undefined' ? window.localStorage : null;
+
+  const [cartProducts, setCartProducts] = useState([]);
+
+  // if we have products in cart state we save them in localstorage
+  useEffect(() => {
+    if (cartProducts.length > 0) {
+      localStorage.setItem('cart', JSON.stringify(cartProducts));
+    }
+  }, [cartProducts]);
+
+  // if there are products in local storage we put them in cart state
+  useEffect(() => {
+    if (ls && ls.getItem('cart')) {
+      setCartProducts(JSON.parse(ls.getItem('cart')));
+    }
+  }, []);
+
+  function addProduct(productId) {
+    setCartProducts((prev) => [...prev, productId]);
+  }
+
+  function removeProduct(productId) {
+    setCartProducts((prev) => {
+      const pos = prev.indexOf(productId);
+      if (pos !== -1) {
+        return prev.filter((value, index) => index !== pos);
+      }
+      return prev;
+    });
+  }
+
+  return (
+    <CartContext.Provider
+      value={{ cartProducts, setCartProducts, addProduct, removeProduct }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
