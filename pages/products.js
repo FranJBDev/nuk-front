451886ec5f029b8f968252d54c827403d2523ab4
gandhyman <gandhycoder@gmail.com>
2023-07-19 14:04:05 -0600

    Update: Translate All products title in products page

--- pages/products.js
+++ pages/products.js
@@ -12,7 +12,7 @@ export default function ProductsPage({ products }) {
     <>
       <Header />
       <Center>
-        <Title>All Products</Title>
+        <Title>Todos los productos</Title>
         <ProductsGrid products={products} />
       </Center>
     </>
